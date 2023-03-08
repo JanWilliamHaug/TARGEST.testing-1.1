9665293792e745b2c01916f489b0f4fa6a5598dd
Jan William Haug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-03-07 23:46:09 -0800

    Update Targest2.py

--- Targest2.py
+++ Targest2.py
@@ -36,7 +36,10 @@ import xlwings as xw
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import Gui
+
+
+
+
 
 
 # Set up the logger for catching errors
@@ -236,7 +239,7 @@ def generateReport(): #Will generate the report for tags
             filepath3 = filepath3.split('.', 1)[0] # removes .docx of the file name
             print(filepath3 + " added to the report")
             nameOfDoc = (filepath3 + " added to the report\n")
-            Gui.Txt.insert(tk.END, nameOfDoc) #print in GUI (m = main.py)
+            Txt.insert(tk.END, nameOfDoc) #print in GUI (m = main.py)
             #runner = paragraph.add_run("\n" + "Document Name: " + filepath3 + "\n")
             #runner.bold = True  # makes the header bold
 
@@ -496,15 +499,15 @@ def generateReport2():
 
 
         msg1 = ("\nReport Generated\n")
-        Gui.Txt.insert(tk.END, msg1) #print in GUI
+        Txt.insert(tk.END, msg1) #print in GUI
         msg2 = ("You can now open up your report\n")
-        Gui.Txt.insert(tk.END, msg2) #print in GUI
+        Txt.insert(tk.END, msg2) #print in GUI
         print("Report Generated")
         print("You can now open up your report")
         report3.save('report3.docx')
         toggle_state() #This will enable the getDoc button
         msg3 = ("You can now open up your excel report as well\n")
-        Gui.Txt.insert(tk.END, msg3) #print in GUI
+        Txt.insert(tk.END, msg3) #print in GUI
         print("Excel Report Generated")
         print("You can now open up your excel report as well")
         toggle_state3()
@@ -606,21 +609,21 @@ def orphanGenReport():
                         i += 1
 
         msg1 = ("\nReport Generated\n")
-        Gui.Txt.insert(tk.END, msg1) #print in GUI
+        Txt.insert(tk.END, msg1) #print in GUI
         msg2 = ("You can now open up your report\n")
-        Gui.Txt.insert(tk.END, msg2) #print in GUI
+        Txt.insert(tk.END, msg2) #print in GUI
         print("Report Generated")
         print("You can now open up your report")
         orphanReport.save('orphanReport.docx')
         toggle_state() #This will enable the getDoc button
         msg3 = ("You can now open up your excel report as well\n")
-        Gui.Txt.insert(tk.END, msg3) #print in GUI
+        Txt.insert(tk.END, msg3) #print in GUI
         print("Excel Report Generated")
         print("You can now open up your excel report as well")
         toggle_state3()
         toggle_state4()
         msgOrphan = ("Orphan report created\n")
-        Gui.Txt.insert(tk.END, msgOrphan) #print in GUI
+        Txt.insert(tk.END, msgOrphan) #print in GUI
         return dicts2Copy
 
     except Exception as e:
@@ -809,20 +812,79 @@ def createExcel():
 
 
 def toggle_state(): # this will re-enable getDoc button
-    Gui.getDoc.config(state="normal")
+    getDoc.config(state="normal")
 
 def toggle_state2(): # this will re-enable generate report button
-    Gui.genRep.config(state="normal")
+    genRep.config(state="normal")
 
 def toggle_state3(): # this will re-enable excel report button
-    Gui.getExcel.config(state="normal")
+    getExcel.config(state="normal")
 
 def toggle_state4(): # this will re-enable word report button for orphan tags
-    Gui.getOrphanDoc.config(state="normal")
+    getOrphanDoc.config(state="normal")
 
 def toggle_state5(): # this will re-enable excel report button for orphan tags
-    Gui.getOrphan.config(state="normal")
+    getOrphan.config(state="normal")
 
 def toggle_state6(): # this will re-enable allTags report button for tables
-    Gui.allTagsButton.config(state="normal")
+    allTagsButton.config(state="normal")
+
+def GUI1():
+    try:
+        # pdb.set_trace()
+        # Creates a word document, saves it as "report 3, and also adds a heading
+        
+        # Creates the gui
+        window = Tk(className=' TARGEST v.1.5.x ')
+        # set window size #
+        window.geometry("500x500")
+        window['background'] = '#afeae6'
+
+        # Creates button 1
+        Button(window, text="Choose list of Documents", command=generateReport, width = 26).pack()
+        # Creates button 2
+        global genRep
+        genRep = Button(window, text="Generate Reports ", state= DISABLED, command=generateReport2, width = 26)
+        genRep.pack()
+        global allTagsButton
+        allTagsButton = Button(text="Open all tags Report", state= DISABLED, command=getDocumentTable, width = 26)
+        allTagsButton.pack()
+        # Creates button 3
+        global getDoc
+        getDoc = Button(window, text="Open Generated Report", state= DISABLED, command=getDocument, width = 26)
+        getDoc.pack()
+        # Creates Excel button button 4
+        global getExcel
+        getExcel = Button(text="Open Generated Excel Report", state= DISABLED, command=createExcel, width = 26)
+        getExcel.pack()
+        # Creates button 5
+        global getOrphan
+        getOrphan = Button(text="Generate Orphan Report", state= DISABLED, command=orphanGenReport, width = 26)
+        getOrphan.pack()
+        # Creates button 6
+        global getOrphanDoc
+        getOrphanDoc = Button(text="Open Orphan Tags Report", state= DISABLED, command=getOrphanDocument, width = 26)
+        getOrphanDoc.pack()
+        
+        # Creates button 7
+        global button
+        button = Button(text="End Program", command=window.destroy, width = 26)
+        button.pack()
+
+        # Create text widget and specify size.
+        global Txt
+        Txt = Text(window, height = 25, width = 55)
+        Txt.pack()
+
+        msg3 = ('You need a text file with paths to your documents\n 1. Please choose your documents by clicking on \n    the "choose document" button.\n 2. Click "Generate Reports".  \n\n')
+        Txt.insert(tk.END, msg3) #print in GUI
+        
+    except Exception as e:
+        # Log an error message
+        logging.exception('main(): ERROR', exc_info=True)
+    else:
+        # Log a success message
+        logging.info('main(): PASS')
+
+        window.mainloop()
 
