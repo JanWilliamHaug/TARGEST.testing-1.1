1d297f3d2850162e3078451936e27a951d5c46c2
Jan William Haug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-03-11 13:48:25 -0800

    removed GUI, and displays everything inTheterminal

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 import Targest2 # Imports the Gui.py file
 
 if __name__ == "__main__" :
-    Targest2.GUI1() # Calls the function GUI1() from the Gui.py file
+    Targest2.generateReport() # Calls the generateReport function from the Gui.py file
+    Targest2.generateReport2() # Calls the generateReport2 function from the Gui.py file
+    Targest2.getDocumentTable() 
+    Targest2.getDocument()
+    Targest2.createExcel()
+    Targest2.orphanGenReport()
+    Targest2.getOrphanDocument()
+
