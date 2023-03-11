2b7db3ada1bba44788df8fd73ff9a7a2fd7579d2
adrian <65195311+adrianb896@users.noreply.github.com>
2023-03-11 14:09:06 -0800

    unit test in main
    push commit

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-#import Targest2
 import Targest2 # Imports the Gui.py file
 
+'''
+import unittest
+import docx
+import re
+
+class TestTags(unittest.TestCase):
+    def test_tags(self):
+        doc = docx.Document('report3.docx')
+        fullText = ''
+        for para in doc.paragraphs:
+            fullText += para.text
+
+            pattern = r'\[([^\]]+)\]'
+            # pattern = r"\b[A-Z]+:\b\d+"
+            matches = re.findall(pattern, fullText)
+            self.assertGreater(len(matches), 0, "No tags found in the report")
+            # self.assertEqual(Targest2.tags("test"), "test")
+            self.assertDictEqual(Targest2.tags(fullText), matches)
+
+def run_tests():
+    suite = unittest.TestSuite()
+    suite.addTest(TestTags('test_tags'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+'''
+
+
 if __name__ == "__main__" :
+    # run_tests()
     Targest2.generateReport() # Calls the generateReport function from the Gui.py file
     Targest2.generateReport2() # Calls the generateReport2 function from the Gui.py file
     Targest2.getDocumentTable() 
