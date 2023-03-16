a8f604fcaeb80034970b0c2426862a61308247a5
Jan William Haug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-03-16 15:00:36 -0700

    newest main

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 import Targest2 # Imports the Gui.py file
 
-'''
+
+
 import unittest
 import docx
 import re
-
 class TestTags(unittest.TestCase):
     def test_tags(self):
         doc = docx.Document('report3.docx')
         fullText = ''
         for para in doc.paragraphs:
             fullText += para.text
-
             pattern = r'\[([^\]]+)\]'
             # pattern = r"\b[A-Z]+:\b\d+"
             matches = re.findall(pattern, fullText)
             self.assertGreater(len(matches), 0, "No tags found in the report")
             # self.assertEqual(Targest2.tags("test"), "test")
             self.assertDictEqual(Targest2.tags(fullText), matches)
-
 def run_tests():
     suite = unittest.TestSuite()
     suite.addTest(TestTags('test_tags'))
     runner = unittest.TextTestRunner()
     runner.run(suite)
-'''
+
 
 
 if __name__ == "__main__" :
     
-    Targest2.generateReport() # Calls the generateReport function from the Gui.py file
-    Targest2.generateReport2() # Calls the generateReport2 function from the Gui.py file
-    Targest2.getDocumentTable() 
-    Targest2.getDocument()
-    Targest2.createExcel()
-    Targest2.orphanGenReport()
-    Targest2.getOrphanDocument()
+    Targest2.generateReport() # Calls the generateReport 
+    Targest2.generateReport2() # Calls the generateReport2 function 
+    #Targest2.getDocumentTable() 
+    #Targest2.getDocument()
+    #Targest2.createExcel()
+    #Targest2.orphanGenReport()
+    #Targest2.getOrphanDocument()
     # run_tests()
-
